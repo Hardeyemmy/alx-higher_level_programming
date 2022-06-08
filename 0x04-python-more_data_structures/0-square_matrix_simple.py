a575d1b2e6e76f863607500f71487c793e7820c1
Hardeyemmy <iiadeyemi307@gmail.com>
2022-06-08 04:19:48 -0700

    1 search replace

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    if not matrix:
-        print()
-    return [[item**2 for item in row] for row in matrix]
+def search_replace(my_list, search, replace):
+    return [find if find != search else replace for find in my_list]
