e1d26cac17289224a52462e8d4a13efb42fae8c8
Hardeyemmy <iiadeyemi307@gmail.com>
2022-07-04 05:36:09 -0700

    1 my list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-MyList = __import__('1-my_list').MyList
 
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted(9)
-print(my_list)
+"""
+    1-my_list: class MyList
+"""
+
+
+class MyList(list):
+    """
+        This class inherits from list.
+        Attributes:
+        Methods:
+            print_sorted - prints the list in ascending order
+    """
+    def print_sorted(self):
+        """
+           prints a list in ascending order.
+        """
+        print(sorted(self))
