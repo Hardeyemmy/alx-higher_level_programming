eefba928cd3fd14136c380e5edce57603b887e01
Hardeyemmy <iiadeyemi307@gmail.com>
2022-09-05 09:34:43 -0700

    6 multi languages loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,7 @@
 // Uses an array to loop through and print strings
 
 const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < langs.length; i++){
+for (let i = 0; i < langs.length; i++)
+{
   console.log(langs[i]);
 }
