bc7213306204b0db5bb9f91312adcafe8c9191ed
Hardeyemmy <iiadeyemi307@gmail.com>
2022-09-05 04:39:40 -0700

    1 multi languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
 //Script that print out 3 different languages
-
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log("C is fun" + '\n' + "Python is cool" + '\n' + "JavaScript is amazing");
