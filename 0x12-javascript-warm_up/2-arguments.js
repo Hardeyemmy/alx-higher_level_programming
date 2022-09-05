84e5a51c557431760e4f947f96252e4cdd145908
Hardeyemmy <iiadeyemi307@gmail.com>
2022-09-05 04:59:22 -0700

    2 arguement

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // Prints a message depending on number of arguments passed
-
-const args = process.argv;
-if (args.length === 0) {
-  console.log('No argument');
-} else if (args.length > 0) {
+const argc = process.argv.length;
+if (argc > 3) {
+  console.log('Arguments found');
+} else if (argc === 3) {
   console.log('Argument found');
 } else {
-  console.log('Arguments found');
+  console.log('No argument');
 }
+
