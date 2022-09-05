1f6453ea0e69e11a6085699f4ee2447e8301f9a3
Hardeyemmy <iiadeyemi307@gmail.com>
2022-09-05 04:03:33 -0700

    1 multi languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*Script that print out 3 different languages*/
+
+const firstLine = "C is fun";
+const secondLine = "Python is cool";
+const thirdLine = "JavaScript is amazing";
+
+console.log(firstLine + '\n' + secondLine + '\n' + thirdLine);
