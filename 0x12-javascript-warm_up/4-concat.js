4c8e64c146ccbecc4220d4c4f8c35fc008e589c8
Hardeyemmy <iiadeyemi307@gmail.com>
2022-09-05 05:31:21 -0700

    4 concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-//Script that print two arguments passed into in 
-const argc = process.argv;
-console.log(argc[2] + ' is ' + argc[3]);
+//Script that print two arguments passed into it with "is" in between
+
+
+const args = process.argv;
+console.log(args[2] + ' is ' + args[3]);
