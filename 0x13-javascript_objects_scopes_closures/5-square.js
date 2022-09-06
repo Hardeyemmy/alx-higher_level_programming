983cbbc4779feefd68f9cfa139d4a234a7a64446
Hardeyemmy <iiadeyemi307@gmail.com>
2022-09-06 09:16:08 -0700

    exec files

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-// Square class that inherits from the Rectangle class
+// class Square that defines a square and inherits from Rectangle of 4-rectangle.js
 
 const Rectangle = require('./4-rectangle');
 
-module.exports = class Square extends Rectangle {
+class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-};
+}
+
+module.exports = Square;
