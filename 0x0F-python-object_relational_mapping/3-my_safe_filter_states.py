ab480985e5c28b3a27cda071aeac9f643f9641c6
Hardeyemmy <iiadeyemi307@gmail.com>
2022-09-11 00:19:27 -0700

    3 my safe filter states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument safe with mySQL injections """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE = %s ORDER BY id ASC", (argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+        cursor.close()
+        db.close()
